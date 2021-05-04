69b5bf4f8bc39fb9e4519ca3e6c7d479ed65fd00
Nguyen Quoc Khanh <nguyenkhanhcv@gmail.com>
2021-05-04 15:51:32 +0700

    Initial Commit

---
+++ src/main/java/com/ptit/commmercialptit/CommmercialPtitApplication.java
@@ -0,0 +1,13 @@
+package com.ptit.commmercialptit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommmercialPtitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommmercialPtitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ptit/commmercialptit/CommmercialPtitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptit.commmercialptit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommmercialPtitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
